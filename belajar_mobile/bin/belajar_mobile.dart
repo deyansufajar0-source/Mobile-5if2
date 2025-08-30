8856ee84064d855b65150b1432f9f0b5e5607c0a
Deyan Sufajar <deyansufajar0@gmail.com>
2025-08-30 20:15:06 +0900

    Upload Pertama Project

---
+++ belajar_mobile/bin/belajar_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_mobile/belajar_mobile.dart' as belajar_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_mobile.calculate()}!');
+}
---
+++ belajar_mobile/test/belajar_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_mobile/belajar_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
